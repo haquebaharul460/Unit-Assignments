5ca13f31fc3b88745532b26d48c6e77495243b7d
haquebaharul460 <haquebaharul460@gmail.com>
2023-03-29 16:40:31 +0530

    initial commit

---
+++ ch 02 - L03/age.js
@@ -0,0 +1,14 @@
+/* 
+create variable age and assign any integer
+if age is more than 18 then print "yes" else print "no"
+*/
+
+
+let age = 21;
+
+if(age>18){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
